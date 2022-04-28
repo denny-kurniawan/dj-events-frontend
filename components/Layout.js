df0c48c30075c43b28330d1bfaaf40b33ed909b5
Denny Kurniawan <dennykurniawanjr@gmail.com>
2022-04-29 05:42:34 +0700

    Header & Footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Layout.module.css'
+import Footer from './Footer'
+import Header from './Header'
 
 const Layout = ({ title, keywords, description, children }) => {
   return (
@@ -10,7 +12,11 @@ const Layout = ({ title, keywords, description, children }) => {
         <meta name='keywords' content={keywords} />
       </Head>
 
+      <Header />
+
       <div className={styles.container}>{children}</div>
+
+      <Footer />
     </div>
   )
 }
