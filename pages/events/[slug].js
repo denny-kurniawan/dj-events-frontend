a8b195f71c53fa352b4cf37bdc16facaea510b73
Denny Kurniawan <dennykurniawanjr@gmail.com>
2022-05-03 14:03:48 +0700

    getStaticPaths

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,10 +1,48 @@
 import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
 
-const Event = () => {
+const Event = ({ evt }) => {
   return (
     <Layout title='Event'>
-      <h1>Event</h1>
+      <h1>{evt.name}</h1>
     </Layout>
   )
 }
 export default Event
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  const paths = events.map((evt) => ({
+    params: { slug: evt.slug },
+  }))
+
+  return {
+    paths,
+    fallback: true,
+  }
+}
+
+export const getStaticProps = async ({ params: { slug } }) => {
+  const res = await fetch(`${API_URL}/api/events/${slug}`)
+  const events = await res.json()
+
+  return {
+    props: {
+      evt: events[0],
+    },
+    revalidate: 1,
+  }
+}
+
+// export const getServerSideProps = async ({ query: { slug } }) => {
+//   const res = await fetch(`${API_URL}/api/events/${slug}`)
+//   const events = await res.json()
+
+//   return {
+//     props: {
+//       evt: events[0],
+//     },
+//   }
+// }
